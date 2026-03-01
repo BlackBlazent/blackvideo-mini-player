fc8f258216b5512d0479ee3376601055ae13ab2d
LoneStamp <nissandiezonencaboagripoz9@gmail.com>
2026-03-01 15:43:19 +0800

    .

--- src/audio.ads
+++ src/audio.ads
@@ -1,10 +1,16 @@
 -- audio.ads
+with System;
+
 package Audio is
-   procedure Init    (Volume : Integer);
+   procedure Init        (Volume : Integer);
    procedure Start;
    procedure Pause;
    procedure Resume;
    procedure Stop;
-   procedure Set_Volume   (Volume : Integer);
+   procedure Set_Volume  (Volume : Integer);
    procedure Toggle_Mute;
+
+   -- Push decoded S16 stereo PCM into the playback ring buffer.
+   -- Called from video_decoder after swr_convert produces S16 output.
+   procedure Push_Audio (Data : System.Address; N_Bytes : Integer);
 end Audio;
